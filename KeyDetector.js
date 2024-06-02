74b446e26e22530924a7f2fd146d00f6f71eec39
Universal UB <115511537+uniub@users.noreply.github.com>
2024-06-02 16:08:19 -0400

    Add files via upload

---
+++ KeyDetector.js
@@ -0,0 +1,36 @@
+// KeyBoardInput
+//
+// Version 1.0
+// Made by Jobi#8313
+//
+// Feel free to modify, edit, and use!
+
+const keyDetector = {
+
+    keydown: '',
+    
+    init: function(){
+        document.addEventListener("keydown", function(event){
+            keyDetector.keydown = event.key
+        })
+        document.addEventListener("keyup", function(event){
+            keyDetector.keydown = ''
+        })
+    },
+
+    isKeyDown: function(input){
+        if (input == this.keydown){
+            return true
+        }
+        return false
+    },
+
+    isKeyUp: function(input){
+        if (input != this.keydown){
+            return true
+        }
+        return false
+    }
+}
+
+export const keyDetector
